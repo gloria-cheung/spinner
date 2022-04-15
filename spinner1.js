847b8f5f37cbf5fffd1c563ce5f7974adff70e9c
Gloria <gloriacheung812@gmail.com>
2022-04-15 23:51:00 +0000

    perform eslint on file

--- spinner1.js
+++ spinner1.js
@@ -12,7 +12,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
@@ -29,15 +29,15 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 1500);
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r|   '); 
+  process.stdout.write('\r|   ');
 }, 1700);
 
 setTimeout(() => {
-  process.stdout.write('\n'); 
+  process.stdout.write('\n');
 }, 1900);
 
